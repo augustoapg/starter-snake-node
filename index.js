56a514dedbd0040a2e3c9df5cd38976093d182a5
Augusto Peres <augustoapg@gmail.com>
2020-06-20 16:46:22 -0400

    added temporary code to bypass new implementation

--- index.js
+++ index.js
@@ -203,8 +203,9 @@ function canMoveDirection(direction, board, mySnake) {
   }
 
   if (isSpaceEmpty(board['snakes'], nextDestination)) {
-    console.log(`The space is empty. It is a possibility.`)
-    return willSnakeBeFree(direction, board, mySnake);
+    console.log(`The space is empty. It is a possibility.`);
+    return true;
+    // return willSnakeBeFree(direction, board, mySnake);
   }
 
   return false;
